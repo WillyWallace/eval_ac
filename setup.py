8ffffb0bd7a9f13cbf8eefb0dc29e67956172446
Andreas Foth <45787527+WillyWallace@users.noreply.github.com>
2022-02-11 15:51:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,5 @@ setup(name='read_and_visualize_abscal-his',
       url='https://github.com/WillyWallace/read_and_visualize_abscal-his.git',
       license='MIT',
       packages=[],
-      install_requires=[
-          'torch', 
-          'xarray',
-          'rpgpy',
-          'toml',
-      ],
+      install_requires=[numpy>=1.22.2, xarray>=0.21.1, datetime, matplotlib ],
      )
