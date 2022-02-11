a74b3c904b748b0570aa7418e0726b28042486a5
Andreas Foth <45787527+WillyWallace@users.noreply.github.com>
2022-02-11 15:56:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,4 +12,12 @@ setup(name='read_and_visualize_abscal-his',
       packages=find_packages(),
       python_requires=>=3.6,
       install_requires=[numpy>=1.22.2, xarray>=0.21.1, datetime, matplotlib, collections],
+      classifiers=[
+        "Development Status :: 1 - Beta",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
      )
