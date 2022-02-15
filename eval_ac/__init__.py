694e792917cdf7d0e83c5920e49baea42c895d95
Andreas Foth <45787527+WillyWallace@users.noreply.github.com>
2022-02-15 14:30:55 +0100

    Create __init__.py

---
+++ eval_ac/__init__.py
@@ -0,0 +1,7 @@
+"""
+evaluate the absolute calibration with liquid nitrogen of an 
+Radiometer Physics GmbH (RPG) microwave radiometer HATPRO.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Andreas Foth'
