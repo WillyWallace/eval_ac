77cd104d51384c2731d7cfae5ce189fe883f434e
Andreas Foth <andreas.foth@uni-leipzig.de>
2022-06-07 17:25:05 +0200

    add first unit test

--- tests/test_convert_abscal_hist.py
+++ tests/test_convert_abscal_hist.py
@@ -1,30 +1,17 @@
 """
 this file contains the unit test routines
 """
-<<<<<<< HEAD
 # import pytest
 from eval_ac.convert_abscal_his import HatproBinAbscalHis
-=======
-import pytest
-from eval_ac import convert_abscal_his
->>>>>>> 7f6339c (add first unit test)
 
 
-FILE = '../example_data/ABSCAL.HIS'
+file = '../example_data/ABSCAL.HIS'
 
-<<<<<<< HEAD
 
-def test_hatpro_bin_abscal_his():
-    '''unit test of the HatproBinAbscalHis class'''
-    obj = HatproBinAbscalHis(FILE)
-    assert obj.filename == FILE
+def test_HatproBinAbscalHis():
+    obj = HatproBinAbscalHis(file)
+    assert obj.filename == file
     assert 'gain' in obj.xrdata == True
     assert 'temp_noise' in obj.xrdata == True
     assert 'temp_sys' in obj.xrdata == True
     assert 'alpha' in obj.xrdata == True
-=======
-def test_HatproBinAbscalHis(FILE):
-    obj = convert_abscal_his.HatproBinAbscalHis(FILE)
-    assert obj.filename == 'ABSCAL.HIS'
-
->>>>>>> 7f6339c (add first unit test)
