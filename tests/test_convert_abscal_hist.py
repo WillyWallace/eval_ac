0789f42c751f53a4473e3715a9d479b9b2f0b76f
Andreas Foth <45787527+WillyWallace@users.noreply.github.com>
2023-05-17 12:05:15 +0200

    Update test_convert_abscal_hist.py

--- tests/test_convert_abscal_hist.py
+++ tests/test_convert_abscal_hist.py
@@ -11,8 +11,8 @@ FILE = '../example_data/ABSCAL.HIS'
 def test_hatpro_bin_abscal_his():
     '''unit test for main class'''
     obj = HatproBinAbscalHis(FILE)
-    assert obj.filename == FILE
-    assert ('gain' in obj.xrdata) == True
-    assert ('temp_noise' in obj.xrdata) == True
-    assert ('temp_sys' in obj.xrdata) == True
-    assert ('alpha' in obj.xrdata) == True
+    assert obj.filename is FILE
+    assert ('gain' in obj.xrdata) is True
+    assert ('temp_noise' in obj.xrdata) is True
+    assert ('temp_sys' in obj.xrdata) is True
+    assert ('alpha' in obj.xrdata) is True
